c8a87d7f42a48b12f662e35925bf3ae482cb69c3
Nirmal Mekala <ndmekala@icloud.com>
2021-05-17 15:36:13 -0500

    components reading fen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Chessboard } from "cm-chessboard"
+import "../styles/cm-chessboard.css"
 
 const Card = (props) => {
 
     useEffect(() => {
-        new Chessboard(document.getElementById(props.data.opening), {position: "start", sprite: {url: "../assets/images/chessboard-sprite.svg"}})
+        new Chessboard(document.getElementById(props.data.opening), {position: props.data.fen, sprite: {url: "./chessboard-sprite.svg"}})
       }, [props]);
 
     return (
         <div style={{border: "1px solid black", margin: "5px", width: "400px"}}>
             <ul>
-                <li>{props.data.opening}</li>
                 <li>{props.data.fullTitle}</li>
                 <li>{props.data.fen}</li>
             </ul>
